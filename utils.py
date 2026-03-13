db5622a2194b22d959f4ea3efd762d471a96e87e
MK <kholodenkom@my.lanecc.edu>
2026-03-13 13:16:13 -0800

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-def get_choice(valid_choices):
-    choice = input("> ").strip()
-
-    while choice not in valid_choices:
-        print("Please choose a valid option.")
-        choice = input("> ").strip()
-
-    return choice
-    
